7f8bf57d22937614686b5957f4ac91d4257d2156
liedekef <liedekef@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-04 15:44:31 +0000

    getting ready for 3.1.5
    git-svn-id: https://plugins.svn.wordpress.org/events-manager-extended/trunk@296538 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- events-manager.php
+++ events-manager.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Events Manager Extended
-Version: 3.1.4
+Version: 3.1.5
 Plugin URI: http://www.e-dynamics.be/wordpress
 Description: Manage events specifying precise spatial data (Location, Town, etc).
 Author: Franky Van Liedekerke
