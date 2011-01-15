f705afb41312474b2dc7cddcd603c4d65ca12730
liedekef <liedekef@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-15 16:34:46 +0000

    more operators for conditional tags
    git-svn-id: https://plugins.svn.wordpress.org/events-manager-extended/trunk@332943 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- eme_functions.php
+++ eme_functions.php
@@ -1,9 +1,15 @@
 <?php
 
 function eme_if_shortcode($atts,$content) {
-   extract ( shortcode_atts ( array ('tag' => '', value => '' ), $atts ) );
+   extract ( shortcode_atts ( array ('tag' => '', 'value' => '', 'notvalue' => '', 'lt' => '', 'gt' => '' ), $atts ) );
    if (!empty($value)) {
       if ($tag===$value) return $content;
+   } elseif (!empty($notvalue)) {
+      if ($tag!==$value) return $content;
+   } elseif (!empty($lt)) {
+      if ($tag<$lt) return $content;
+   } elseif (!empty($gt)) {
+      if ($tag>$gt) return $content;
    } else {
       if (!empty($tag)) return $content;
    }
