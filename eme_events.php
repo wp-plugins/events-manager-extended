a8617165a50b8baebfcb9c7eeb8f63d7f13dc769
liedekef <liedekef@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-22 12:18:41 +0000

    git-svn-id: https://plugins.svn.wordpress.org/events-manager-extended/trunk@335749 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- eme_events.php
+++ eme_events.php
@@ -419,7 +419,7 @@ function eme_options_subpanel() {
    eme_options_radio_binary ( __ ( 'Enable Google Maps integration?' ), 'eme_gmap_is_active', __ ( 'Check this option to enable Google Map integration.','eme' ) );
    eme_options_radio_binary ( __ ( 'Include Google Maps JS in header?' ), 'eme_google_maps_js_in_header', __ ( 'Some themes are badely designed and can have issues showing the google maps. If so, try activating this option which will cause the Google Maps Javascript to always be included in the header of every page (off by default).','eme' ) );
    eme_options_radio_binary ( __ ( 'Delete all EME data when uninstalling?', 'eme' ), 'eme_uninstall_drop_data', __ ( 'Check this option if you want to delete all EME data (database tables and options) when deactivating the plugin.', 'eme' ) );
-   eme_options_radio_binary ( __ ( 'Enable shortcodes in widgets', 'eme' ), 'eme_shortcodes_in_widgets', __ ( 'Check this option if you want to enale the use of shortcodes in widgets (affects shortcodes of any plugin used in widgets, so use with care).', 'eme' ) );
+   eme_options_radio_binary ( __ ( 'Enable shortcodes in widgets', 'eme' ), 'eme_shortcodes_in_widgets', __ ( 'Check this option if you want to enable the use of shortcodes in widgets (affects shortcodes of any plugin used in widgets, so use with care).', 'eme' ) );
    ?>
 </table>
 <h3><?php _e ( 'Events page', 'eme' ); ?></h3>
